36296b9d9c30fc554c9936235d46844a141f24e5
carla <carlajilo@gmail.com>
2023-12-11 15:08:16 +0100

    database conection

---
+++ lib/DB/db.php
@@ -0,0 +1,21 @@
+<?php
+
+class DB {
+    private $pdo;
+
+
+    public function __construct() {
+        require $_SERVER['DOCUMENT_ROOT'].'/Videoclub/lib/Config/config.php';
+        
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $usuario, $pwd);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+    
+    public function getPDO() {
+        return $this->pdo;
+    }
+}
